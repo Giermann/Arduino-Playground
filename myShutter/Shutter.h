4ec0cfdf7325563e2a7ba85538b1b3b752ea9745
Sven Giermann <sven.giermann@gmail.com>
2016-06-14 09:30:05 +0200

    Update Shutter.h
    code cleanup, simulate mode (20160607)

--- myShutter/Shutter.h
+++ myShutter/Shutter.h
@@ -2,86 +2,84 @@
 #define __SHUTTER_H
 
 #ifdef _WINDOWS
-#define LED_ON HIGH
-#define LED_OFF LOW
+  #define LED_ON HIGH
+  #define LED_OFF LOW
 
-#define relayPin1      4      // the pin numbers for all 4 relays
-#define _DEBUG
-//#define _BLINK
-#ifdef _BLINK
-#define led_PIN        3
-#define relayPin2      1
-#define relayPin3      2
-#else
-#define relayPin2      3
-#define relayPin3      1
-#endif
-#define relayPin4      2
-#define buttonAPin     0      // the number of the pin for push button(s) ADC (ADC0 = 14 .. ADC5 = 19)
-#define buttonDPin     5      // the number of the pin for push button(s) digital state
-#define onewirePin     0      // the number of the pin for 1-Wire Slave
-#define onewireClnt 0x0C
-#elif defined( ARDUINO_attiny )
-//#if defined( __AVR_ATtiny25__ ) | \
+  #define relayPin1      4      // the pin numbers for all 4 relays
+  #define _DEBUG
+  //#define _BLINK
+  #ifdef _BLINK
+    #define led_PIN      3
+    #define relayPin2    2
+  #else
+    #define relayPin2    3
+  #endif
+  #define relayPin3      1
+  #define relayPin4      2
+  // below are dummies for Windows
+  #define buttonAPin     0
+  #define buttonDPin     5
+  #define onewirePin     0
+  #define onewireClnt 0x0C
+//#elif defined( __AVR_ATtiny25__ ) | \
 //     defined( __AVR_ATtiny45__ ) | \
 //     defined( __AVR_ATtiny85__ )
-//#define _OW_SLAVE
-//#define _I2C_SLAVE
-//#define _BS_SLAVE
+#elif defined( ARDUINO_attiny )
+  // The led is connected so that the tiny sinks current
+  #define LED_ON  LOW
+  #define LED_OFF HIGH
 
-// The led is connected so that the tiny sinks current
-#define LED_ON LOW
-#define LED_OFF HIGH
+  #define relayPin1      4      // the pin numbers for all 4 relays
+  //#define _BLINK
+  #ifdef _BLINK
+    #define led_PIN      3
+    #define relayPin2    2
+  #else
+    #define relayPin2    3
+  #endif
+  #define relayPin3      1
+  #define relayPin4      2
+  #define buttonAPin     0      // the number of the pin for push button(s) ADC (ADC0 = 6, ADC1 = 2, ADC2 = 4, ADC3 = 3)
+  #define buttonDPin     5      // the number of the pin for push button(s) digital state
+  #define onewirePin     0      // the number of the pin for 1-Wire Slave
+  #define onewireClnt 0x0C
+#else // Arduino Nano
+  #define _DEBUG
+  #define _T(x) (x)
+  //  #define _OW_SLAVE
+  //#define _I2C_SLAVE
+  //#define _BS_SLAVE
 
-#define relayPin1      4      // the pin numbers for all 4 relays
-//#define _BLINK
-#ifdef _BLINK
-#define led_PIN        3
-#define relayPin2      1
-#define relayPin3      2
-#else
-#define relayPin2      3
-#define relayPin3      1
-#endif
-#define relayPin4      2
-#define buttonAPin     0      // the number of the pin for push button(s) ADC (ADC0 = 6, ADC1 = 2, ADC2 = 4, ADC3 = 3)
-#define buttonDPin     5      // the number of the pin for push button(s) digital state
-#define onewirePin     0      // the number of the pin for 1-Wire Slave
-#define onewireClnt 0x0C
-#else
-#define _DEBUG
-#define _T(x) (x)
-//  #define _OW_SLAVE
-//#define _I2C_SLAVE
-//#define _BS_SLAVE
+  #define LED_ON HIGH
+  #define LED_OFF LOW
 
-#define LED_ON HIGH
-#define LED_OFF LOW
-
-#define _BLINK
-#define led_PIN       13
-#define relayPin1      4      // the pin numbers for all 4 relays
-#define relayPin2      3
-#define relayPin3      1
-#define relayPin4      2
-#define buttonAPin     1      // the number of the pin for push button(s) ADC (ADC0 = 14 .. ADC5 = 19)
-#define buttonDPin    15      // the number of the pin for push button(s) digital state
-#define onewirePin    18      // the number of the pin for 1-Wire Slave (2 is working)
-#define onewireClnt 0x0D
+  #define _BLINK
+  #define led_PIN       13
+  #define relayPin1      4      // the pin numbers for all 4 relays
+  #define relayPin2      3
+  #define relayPin3      1
+  #define relayPin4      2
+  #define buttonAPin     1      // the number of the pin for push button(s) ADC (ADC0 = 14 .. ADC5 = 19)
+  #define buttonDPin    15      // the number of the pin for push button(s) digital state
+  #define onewirePin    18      // the number of the pin for 1-Wire Slave (2 is working)
+  #define onewireClnt 0x0D
 #endif
 
-// only used locally, so keep separated from bae910
-uint8_t  tempButton;
-uint8_t  newButton;
 
 #define S_IDLE            0x00
 #define S_MOVE_OPEN       0x01
 #define S_MOVE_CLOSE      0x02
 
+// restore EEPROM to default and set client ID:
+// # ID=85
+// # owwrite /FC.0C0000000000/910/alarm 113 ; owwrite /FC.0C0000000000/910/alarmc $ID ; owwrite /FC.0C0000000000/910/alarm 111 ; sleep 5 ; owdir /uncached ; ID=$(expr $ID + 1) ; echo next ID = $ID
+
 #define crcEEPROM       bae910.memory.field.reserved    // [uint16_t] used for EEPROM saving
 #define clientID        bae910.memory.field.alarmc      // [uint8_t]  1 wire client ID (maybe duty4?)
 #define controlEEPROM   bae910.memory.field.alarm       // [uint8_t] used to (re)store EEPROM data
 #define CTRL_LockButtonMode   66                        //  = lock buttons, only remote control
+#define CTRL_SimulateMode     77                        //  = don't act on relays, but update states and positions
+#define CTRL_RestorePos       78                        //  = restore/set currentPos via 1-Wire after reset condition
 #define CTRL_TestMode         80                        //  = test relay mode (lower 4 bits for relay state: 80..95)
 #define CTRL_SomfySetupMode   96                        //  = enter setup mode (both relays for setupLength, lower 2 bits for window: 97 = 1, 98 = 2, 99 = both)
 #define CTRL_SomfyDefaultRst 100                        //  = reset default config (both relays for 2x setupLength, lower 2 bits for window: 101 = 1, 102 = 2, 103 = both)
@@ -94,7 +92,8 @@ uint8_t  newButton;
 #define internalState1  bae910.memory.field.userc       // [uint8_t]
 #define internalState2  bae910.memory.field.userd
 #define currentButton   bae910.memory.field.pio         // [uint8_t]  1 or 2; 3 = 1+2
-#define lastButtonADC   bae910.memory.field.adc10       // [uint16_t] last ADC value read
+#define tempButton      bae910.memory.field.unused0x35
+#define lastButtonADC   bae910.memory.field.adc10       // [uint16_t, read-only] last ADC value read
 
 #define lastTempButton  bae910.memory.field.count       // [uint32_t]
 #define lastPosChange1  bae910.memory.field.adctotp
@@ -105,18 +104,21 @@ uint8_t  newButton;
 #define lastBtnEvent2   bae910.memory.field.usern
 #define lastLongpress1  bae910.memory.field.usero
 #define lastLongpress2  bae910.memory.field.userp
-#define lastMoveLength1 bae910.memory.field.useri       // [uint16_t] duration of last movement
-#define lastMoveLength2 bae910.memory.field.userj
 #define lastMoveState1  bae910.memory.field.usere       // [uint8_t]  last direction moved
 #define lastMoveState2  bae910.memory.field.userf
-//#ifdef _WINDOWS
-//  #define currentPos1   myGlobalWnd->Position1
-//  #define currentPos2   myGlobalWnd->Position2
-//#else
-  #define currentPos1   bae910.memory.field.adc         // [uint8_t]  0 = opened, 100 = closed
+#define lastMoveLength1 bae910.memory.field.adcap       // [uint16_t, read-only] duration of last movement
+#define lastMoveLength2 bae910.memory.field.adcan
+
+#define invalidPos      101
+#define switchStopPos   150                             // 100 < 'full open first' < switchStopPos < 'full close first'
+#ifdef _WINDOWS
+  #define currentPos1   myGlobalWnd->Position1
+  #define currentPos2   myGlobalWnd->Position2
+#else
+  #define currentPos1   bae910.memory.field.adc         // [uint8_t, read-only]  0 = opened, 100 = closed
   #define currentPos2   bae910.memory.field.cnt
-//#endif
-#define stopPos1        bae910.memory.field.usera       // [uint8_t]  0..100 | 101..200 = move to one end and back to (200 - stopPos)
+#endif
+#define stopPos1        bae910.memory.field.usera       // [uint8_t]  0..100 | 101..200 = move to one end and back to (stopPos - 100)
 #define stopPos2        bae910.memory.field.userb
 
 #define defaultDirOpen  bae910.memory.field.outc        // [uint8_t]  single button mode: default to up direction if position >50%
@@ -132,7 +134,7 @@ uint8_t  newButton;
 
 #define buttonLongpress bae910.memory.field.duty1       // [uint16_t] minimum length for long button press
 #define holdLongpress   bae910.memory.field.duty2       // [uint16_t] hold long press mode after button release for ...ms
-#define directionReset  bae910.memory.field.duty3       // [uint16_t] single button mode: move up on second request, reset to down after 15s
+#define directionReset  bae910.memory.field.duty3       // [uint16_t] single button mode: move up on second request, reset to down after 15s (0 = never)
 #define setupLength     bae910.memory.field.duty4       // [uint16_t] duration for both relays ON to enter setup mode
 #define minADCidle      bae910.memory.field.ovruncnt    // [uint16_t] ADC above this value is being treated as idle
 #define minADCbutton1   bae910.memory.field.maxap       // [uint16_t] ADC above is button 1, below is button 2
@@ -146,6 +148,8 @@ uint8_t  newButton;
     uint32_t unused0x4e;
     uint16_t userl;
     uint16_t userk;
+    uint16_t userj;
+    uint16_t useri;
     uint8_t  userh;
     uint8_t  userg;
     uint16_t maxcps;
@@ -159,13 +163,8 @@ uint8_t  newButton;
     uint16_t pc2;
     uint16_t pc1;
     uint16_t pc0;
-    uint8_t  unused0x35;
     uint16_t cps;   // read-only
-    uint16_t adcan; // read-only
-    uint16_t adcap; // read-only
     uint8_t  pioc   // in (copied) EEPROM!
 */
-
-#define timeDiff(previous) (unsigned long)(currentMillis - previous)
 
 #endif // __SHUTTER_H
