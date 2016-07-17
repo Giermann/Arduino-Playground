bb929173a3f77ef956a61c0c7f48cf9303070546
Sven Giermann <sven.giermann@gmail.com>
2016-07-17 20:59:59 +0200

    20160717

--- myShutter/Shutter.h
+++ myShutter/Shutter.h
@@ -65,6 +65,8 @@
   #define onewireClnt 0x0D
 #endif
 
+// Software version
+#define configVersion     0x02  // returned as SW_VER
 
 #define S_IDLE            0x00
 #define S_MOVE_CLOSE      0x01  // keep S_MOVE in sync with buttons (1 = close, 2 = open) ?
@@ -75,8 +77,6 @@
 // # ID=85
 // # owwrite /FC.0C0000000000/910/alarm 113 ; owwrite /FC.0C0000000000/910/alarmc $ID ; owwrite /FC.0C0000000000/910/alarm 111 ; sleep 5 ; owdir /uncached ; ID=$(expr $ID + 1) ; echo next ID = $ID
 
-#define crcEEPROM       bae910.memory.field.reserved    // [uint16_t] used for EEPROM saving
-#define clientID        bae910.memory.field.alarmc      // [uint8_t]  1 wire client ID (maybe duty4?)
 #define controlEEPROM   bae910.memory.field.alarm       // [uint8_t] used to (re)store EEPROM data
 #define CTRL_LockButtonMode   66                        //  = lock buttons, only remote control
 #define CTRL_SimulateMode     77                        //  = don't act on relays, but update states and positions
@@ -122,6 +122,7 @@
 #define lastMoveState2  bae910.memory.field.userf
 #define lastMoveLength1 bae910.memory.field.adcap       // [uint16_t, read-only] duration of last movement
 #define lastMoveLength2 bae910.memory.field.adcan
+#define relayState      bae910.memory.field.out         // [uint8_t]
 
 #define openPos          30                             // 100 < 'full open first' < 100+openPos < 'full close first'
 #define invalidPos      101
@@ -131,28 +132,32 @@
 #define currentPos2     bae910.memory.field.cnt
 #define stopPos1        bae910.memory.field.usera       // [uint8_t]  0..100 | 101..200 = move to one end and back to (stopPos - 100)
 #define stopPos2        bae910.memory.field.userb
+#define stopPosBoth     bae910.memory.field.userh       // [uint8_t] used to set both stop positions in one turn via 1-Wire
+
+// configuration values
+#define clientID        bae910.memory.field.alarmc      // [uint8_t]  1 wire client ID (maybe duty4?)
 
 #define defaultDirOpen  bae910.memory.field.outc        // [uint8_t]  single button mode: default to up direction if position >30%
-#define positionFactor1 bae910.memory.field.period1     // [uint16_t] time in ms for 1% movement
-#define positionFactor2 bae910.memory.field.period2
-#define relayOffset     bae910.memory.field.pioc        // [uint8_t]  time in ms between relay on and start moving
-
 #define btnOpenPos1     bae910.memory.field.adcc        // [uint8_t]  move to this position on CLOSE button (move full and back if 0x80 bit set)
 #define btnOpenPos2     bae910.memory.field.cntc
 #define btnClosePos1    bae910.memory.field.tpm1c
 #define btnClosePos2    bae910.memory.field.tpm2c
 
-#define buttonSettle    bae910.memory.field.rtcc        // [uint8_t]  state must not change in this time to be treated as valid
+#define posFactorClose1 bae910.memory.field.pc0         // [uint16_t] time in ms for 1% movement
+#define posFactorOpen1  bae910.memory.field.pc1
+#define posFactorClose2 bae910.memory.field.pc2
+#define posFactorOpen2  bae910.memory.field.pc3
+#define posRelayOffset  bae910.memory.field.pioc        // [uint8_t]  time in ms between relay on and start moving
 
+#define buttonSettle    bae910.memory.field.rtcc        // [uint8_t]  state must not change in this time to be treated as valid
 #define buttonLongpress bae910.memory.field.duty1       // [uint16_t] minimum length for long button press
 #define holdLongpress   bae910.memory.field.duty2       // [uint16_t] hold long press mode after button release for ...ms
 #define directionReset  bae910.memory.field.duty3       // [uint16_t] single button mode: move up on second request, reset to down after 15s (0 = never)
 #define setupLength     bae910.memory.field.duty4       // [uint16_t] duration for both relays ON to enter setup mode
 #define minADCidle      bae910.memory.field.ovruncnt    // [uint16_t] ADC above this value is being treated as idle
-#define minADCbutton1   bae910.memory.field.maxap       // [uint16_t] ADC above is button 1, below is button 2
-#define minADCbutton2   bae910.memory.field.maxan       // [uint16_t] ADC above is button 2, below is both buttons pressed
+#define minADCbutton1   bae910.memory.field.period1     // [uint16_t] ADC above is button 1, below is button 2
+#define minADCbutton2   bae910.memory.field.period2     // [uint16_t] ADC above is button 2, below is both buttons pressed
 
-#define relayState      bae910.memory.field.out         // [uint8_t]
 #define relayAssignment bae910.memory.field.stalledcnt  // [uint16_t]
 
 /* unused fields left:
@@ -162,7 +167,6 @@
     uint16_t userk;
     uint16_t userj;
     uint16_t useri;
-    uint8_t  userh;
     uint8_t  userg;
     uint16_t maxcps;
     uint16_t selectcnt;
@@ -171,11 +175,9 @@
     uint16_t alan;
     uint16_t alap;
     //
-    uint16_t pc3;
-    uint16_t pc2;
-    uint16_t pc1;
-    uint16_t pc0;
     uint16_t cps;   // read-only
+    uint16_t maxan;
+    uint16_t maxap;
 */
 
 #endif // __SHUTTER_H
